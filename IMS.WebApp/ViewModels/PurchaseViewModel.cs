46eae717eb1ff959181e01783e2f35d53c1ffbe0
Pavel Gajdusek <pavel@gajdusek.eu>
2023-02-22 17:42:10 +0100

    Add PurchaseInventory page init and PurchaseViewModel class

---
+++ IMS.WebApp/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels;
+
+public class PurchaseViewModel
+{
+    [Required] public string PONumber { get; set; }
+    [Required] public int InventoryId { get; set; }
+    [Required] public int QuantityToPurchase { get; set; }
+    public double InventoryPrice { get; set; }
+}
