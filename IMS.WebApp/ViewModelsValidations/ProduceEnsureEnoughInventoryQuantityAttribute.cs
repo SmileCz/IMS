086da3b392afd088ca90bb295c8a6b3b378c6dfc
Pavel Gajdusek <pavel@gajdusek.eu>
2023-02-25 14:34:31 +0100

    Add Produce product form

---
+++ IMS.WebApp/ViewModelsValidations/ProduceEnsureEnoughInventoryQuantityAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using IMS.WebApp.ViewModels;
+
+namespace IMS.WebApp.ViewModelsValidations;
+
+public class ProduceEnsureEnoughInventoryQuantityAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var produceViewModel = validationContext.ObjectInstance as ProduceViewModel;
+
+        if (produceViewModel?.Product?.ProductInventories == null) return ValidationResult.Success;
+        foreach (var pi in produceViewModel.Product.ProductInventories)
+        {
+            if (pi.Inventory != null &&
+                pi.InventoryQuality * produceViewModel.QuantityToProduce > pi.Inventory.Quantity)
+            {
+                return new ValidationResult(
+                    $"The inventory ({pi.Inventory.Name}) is not enough to produce {produceViewModel.QuantityToProduce} products.",
+                    new[] { validationContext.MemberName });
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
---
+++ IMS.WebApp/wwwroot/JSInterOp.js
@@ -0,0 +1,8 @@
+﻿function preventFormSubmition(formId) {
+    document.getElementById(`${formId}`).addEventListener("keydown", function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            return false;
+        }
+    })
+}
