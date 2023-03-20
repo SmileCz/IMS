8638927c24516928e3d1a883c2561cc17c32fb6b
Pavel Gajdusek <pavel@gajdusek.eu>
2023-03-20 07:56:21 +0100

    Add SQLite

---
+++ IMS.Plugins/IMS.Plugins.EfCoreSQLite/Migrations/20230303082632_Init.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.Plugins.EfCoreSQLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InventoryTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    InventoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    QuantityBefore = table.Column<int>(type: "INTEGER", nullable: false),
+                    QuantityAfter = table.Column<int>(type: "INTEGER", nullable: false),
+                    UnitPrice = table.Column<double>(type: "REAL", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DoneBy = table.Column<string>(type: "TEXT", nullable: false),
+                    PoNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductionNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    ActivityType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InventoryTransactions_Inventories_InventoryId",
+                        column: x => x.InventoryId,
+                        principalTable: "Inventories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductInventories",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    InventoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    InventoryQuality = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductInventories", x => new { x.ProductId, x.InventoryId });
+                    table.ForeignKey(
+                        name: "FK_ProductInventories_Inventories_InventoryId",
+                        column: x => x.InventoryId,
+                        principalTable: "Inventories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductInventories_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    QuantityBefore = table.Column<int>(type: "INTEGER", nullable: false),
+                    QuantityAfter = table.Column<int>(type: "INTEGER", nullable: false),
+                    UnitPrice = table.Column<double>(type: "REAL", nullable: true),
+                    TransactionDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DoneBy = table.Column<string>(type: "TEXT", nullable: false),
+                    SoNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductionNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    ActivityType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductTransactions_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryTransactions_InventoryId",
+                table: "InventoryTransactions",
+                column: "InventoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInventories_InventoryId",
+                table: "ProductInventories",
+                column: "InventoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTransactions_ProductId",
+                table: "ProductTransactions",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InventoryTransactions");
+
+            migrationBuilder.DropTable(
+                name: "ProductInventories");
+
+            migrationBuilder.DropTable(
+                name: "ProductTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ IMS.Plugins/IMS.Plugins.EfCoreSQLite/Migrations/20230303085855_AddSeed.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IMS.Plugins.EfCoreSQLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Inventories",
+                columns: new[] { "Id", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Bike Seat", 2.0, 10 },
+                    { 2, "Bike Body", 15.0, 10 },
+                    { 3, "Bike Wheels", 8.0, 20 },
+                    { 4, "Bike Pedels", 1.0, 20 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Bike", 150.0, 10 },
+                    { 2, "Car", 25000.0, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInventories",
+                columns: new[] { "InventoryId", "ProductId", "InventoryQuality" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 1, 1 },
+                    { 3, 1, 2 },
+                    { 4, 1, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ProductInventories",
+                keyColumns: new[] { "InventoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductInventories",
+                keyColumns: new[] { "InventoryId", "ProductId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductInventories",
+                keyColumns: new[] { "InventoryId", "ProductId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductInventories",
+                keyColumns: new[] { "InventoryId", "ProductId" },
+                keyValues: new object[] { 4, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Inventories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Inventories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Inventories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Inventories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
